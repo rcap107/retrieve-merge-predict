fcdb3d61ae9a551e73952883d77107e8ded6676a
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-05-10 16:15:15 +0200

    Added scripts for debugging sections of the pipeline.

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
-import argparse
+#%% 
+# %load_ext autoreload
+# %autoreload 2
+from src.data_preparation.data_structures import RawDataset
+# %%
+import pandas as pd
+import polars as pl
+from pathlib import Path
 
+# %%
+data_folder = Path("data/yago3-dl/wordnet")
+src_dataset_path = Path(data_folder, "")
+dataset_path  = Path(src_dataset_path, "yago_seltab_wordnet_site.parquet")
+ds = RawDataset(dataset_path, "yago3-dl", "data/metadata")
+# %%
+# ds.save_metadata_to_json()
 
-if __name__ == "__main__":
-    pass
+# %%
+
+ds.prepare_hash("write_json")
+
+ds.prepare_hash("write_ipc")
+ 
+ds.prepare_hash("zlib")
+
+# %%
+
+data_folder = Path("data/source_tables/ken_datasets")
+src_dataset_path = Path(data_folder, "us-accidents")
+dataset_path  = Path(src_dataset_path, "us-accidents.csv")
+ds = RawDataset(dataset_path, "ken_datasets", "data/metadata")
+
+# %%
+%%timeit
+ds.prepare_hash("write_json")
+
+#%%
+%%timeit
+ds.prepare_hash("write_ipc")
+
+#%%
+%%timeit 
+ds.prepare_hash("zlib")
+
+# %%
