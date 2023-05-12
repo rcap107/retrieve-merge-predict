6848cb4fb3ffa3e594c10d3332c11dda9efbad0d
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-05-12 10:10:23 +0200

    More work on the debugging scripts.

--- debug_indexes.py
+++ debug_indexes.py
@@ -4,34 +4,66 @@ from pathlib import Path
 from src.candidate_discovery.utils_minhash import MinHashIndex
 from src.candidate_discovery.utils_lazo import LazoIndex
 import pickle
+import json
+from tqdm import tqdm
+
 # %%
 data_path = Path("data/yago3-dl/wordnet")
+metadata_path = Path("data/metadata")
+df_dict = {}
+
+# %%
+mh_index = MinHashIndex()
+
+# Count the number of total files in the glob
+total_files = sum(1 for f in metadata_path.glob("*.json"))
+
+for path in tqdm(metadata_path.glob("*.json"), total=total_files):
+    mdata_dict = json.load(open(path, "r"))
+    ds_hash = mdata_dict["hash"]
+    df = pl.read_parquet(mdata_dict["full_path"])
+    mh_index.add_single_table(df, ds_hash)
+
+
+# %%
+mh_index.create_ensembles()
+
+# Saving the file
+pickle.dump(obj=mh_index, file=open("mh_index.pickle", "wb"))
+
+# %%
+if False:
+    df_dict = {tab_name: df}
+    all_movie_tables = data_path.glob("**/subtabs/yago_seltab_wordnet_movie/*.parquet")
+    for tab_path in all_movie_tables:
+        tab_name = tab_path.stem
+        print(tab_name)
+        df_dict.update({tab_name: pl.read_parquet(tab_path)})
+    mh_index = MinHashIndex(df_dict)
+    mh_index.create_ensembles()
 # %%
 tab_name = "yago_seltab_wordnet_movie"
 tab_path = Path(data_path, f"{tab_name}.parquet")
 df = pl.read_parquet(tab_path)
 
-df_dict = { tab_name: df}
-# %%
-all_movie_tables = data_path.glob("**/subtabs/yago_seltab_wordnet_movie/*.parquet")
-for tab_path in all_movie_tables:
-    tab_name = tab_path.stem
-    print(tab_name)
-    df_dict.update({tab_name: pl.read_parquet(tab_path)})
-#%%
-mh_index = MinHashIndex(df_dict)
-mh_index.create_ensembles()
-
 # %%
 query = df["isLocatedIn"].sample(50000).drop_nulls()
 mh_result = mh_index.query_index(query)
+pickle.dump({k: v for k, v in enumerate(mh_result)}, open("mh_result.pickle", "wb"))
 
-#%%
+
+############################ LAZO
+# %%
 lazo_index = LazoIndex(df_dict)
 # %%
 lazo_result = lazo_index.query_index(query)
 # %%
-pickle.dump({k: v for k,v in enumerate(mh_result)}, open("mh_result.pickle", "wb"))
-pickle.dump({k: v for k,v in enumerate(lazo_result)}, open("lazo_results.pickle", "wb"))
+pickle.dump(
+    {k: v for k, v in enumerate(lazo_result)}, open("lazo_results.pickle", "wb")
+)
+
+# %%
+# Load index from pickle
+ensembles = pickle.load(open("mh_index.pickle", "rb"))
 
 # %%
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 #%% 
-# %load_ext autoreload
-# %autoreload 2
+%load_ext autoreload
+%autoreload 2
 from src.data_preparation.data_structures import RawDataset
 # %%
 import pandas as pd
 import polars as pl
 from pathlib import Path
-
-# %%
-data_folder = Path("data/yago3-dl/wordnet")
-src_dataset_path = Path(data_folder, "")
-dataset_path  = Path(src_dataset_path, "yago_seltab_wordnet_site.parquet")
-ds = RawDataset(dataset_path, "yago3-dl", "data/metadata")
-# %%
-# ds.save_metadata_to_json()
-
-# %%
-
-ds.prepare_hash("write_json")
-
-ds.prepare_hash("write_ipc")
- 
-ds.prepare_hash("zlib")
-
 # %%
 
 data_folder = Path("data/source_tables/ken_datasets")
@@ -30,16 +13,16 @@ src_dataset_path = Path(data_folder, "us-accidents")
 dataset_path  = Path(src_dataset_path, "us-accidents.csv")
 ds = RawDataset(dataset_path, "ken_datasets", "data/metadata")
 
+
 # %%
-%%timeit
-ds.prepare_hash("write_json")
+data_folder = Path("data/yago3-dl/wordnet")
+src_dataset_path = Path(data_folder, "")
+dataset_path  = Path(src_dataset_path, "yago_seltab_wordnet_site.parquet")
+ds = RawDataset(dataset_path, "yago3-dl", "data/metadata")
 
-#%%
-%%timeit
-ds.prepare_hash("write_ipc")
 
-#%%
-%%timeit 
-ds.prepare_hash("zlib")
-
+# %%
+for dataset_path in data_folder.glob("**/*.parquet"):
+    ds = RawDataset(dataset_path, "yago3-dl", "data/metadata")
+    ds.save_metadata_to_json()
 # %%
