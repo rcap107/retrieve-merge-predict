9aec97ace09764cd883f07faeda8d99ba393db73
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-05-25 11:32:42 +0200

    Updated preparation of metadata.

--- prepare_metadata.py
+++ prepare_metadata.py
@@ -8,33 +8,40 @@ import pandas as pd
 import polars as pl
 from pathlib import Path
 # %%
+def save_single_table(dataset_path, dataset_source, metadata_dest):
+    ds = RawDataset(dataset_path, dataset_source, metadata_dest)
+    ds.save_metadata_to_json()
 
+#%%
+ds_name = "us-accidents"
 data_folder = Path("data/source_tables/ken_datasets")
-src_dataset_path = Path(data_folder, "us-accidents")
-dataset_path  = Path(src_dataset_path, "us-accidents.csv")
-ds = RawDataset(dataset_path, "ken_datasets", "data/metadata")
+src_dataset_path = Path(data_folder, ds_name)
+dataset_path  = Path(src_dataset_path, ds_name + ".csv")
 
+save_single_table(dataset_path, "ken_datasets", "data/metadata/sources")
 
-# %%
-data_folder = Path("data/yago3-dl/wordnet")
-src_dataset_path = Path(data_folder, "")
-dataset_path  = Path(src_dataset_path, "yago_seltab_wordnet_site.parquet")
-ds = RawDataset(dataset_path, "yago3-dl", "data/metadata")
+#%%
+ds_name = "movies-prepared"
+data_folder = Path("data/source_tables/ken_datasets")
+src_dataset_path = Path(data_folder, "the-movies-dataset")
+dataset_path  = Path(src_dataset_path, ds_name + ".parquet")
 
+save_single_table(dataset_path, "ken_datasets", "data/metadata/sources")
 
-# %%
-for dataset_path in data_folder.glob("**/*.parquet"):
-    ds = RawDataset(dataset_path, "yago3-dl", "data/metadata")
-    ds.save_metadata_to_json()
-# %%
+#%%
+for case in ["wordnet", "binary", "seltab"]:
+    data_folder = Path(f"data/yago3-dl/{case}")
+    if data_folder.exists():
+        for dataset_path in data_folder.glob("**/*.parquet"):
+            ds = RawDataset(dataset_path, "yago3-dl", f"data/metadata/{case}")
+            ds.save_metadata_to_json(f"data/metadata/{case}")
+            ds.save_metadata_to_json("data/metadata/full")
+    else:
+        raise FileNotFoundError(f"Invalid path {data_folder}")
 
-############# Prepare debug subset
-data_folder = Path("data/yago3-dl/wordnet/subtabs/yago_seltab_wordnet_movie")
-for dataset_path in data_folder.glob("**/*.parquet"):
-    ds = RawDataset(dataset_path, "yago3-dl", "data/metadata/debug")
-    ds.save_metadata_to_json()
-
-# %%
-metadata_index = MetadataIndex("data/metadata/debug")
-metadata_index.save_index("debug_metadata_index.pickle")
-# %%
+#%% 
+# Prepare metadata indices
+for case in ["wordnet", "binary", "seltab", "full"]:
+    metadata_index = MetadataIndex(f"data/metadata/{case}")
+    metadata_index.save_index(f"data/metadata/mdi/md_index_{case}.pickle")
+    
