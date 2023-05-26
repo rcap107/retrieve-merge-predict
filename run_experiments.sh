e4ee36dde37e8c235810ec7b50ecf0466535a886
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-05-26 22:54:45 +0200

    Updated experiment script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-python main_pipeline.py --metadata_dir data/metadata/binary --metadata_index data/metadata/mdi/md_index_binary.pickle --index_dir data/metadata/indices \
---source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 10
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 wordnet
 
-python main_pipeline.py --metadata_dir data/metadata/binary --metadata_index data/metadata/mdi/md_index_binary.pickle --index_dir data/metadata/indices \
---source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 10
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 wordnet
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 wordnet
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 binary
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 binary
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 binary
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 seltab
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 seltab
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 seltab
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 full
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 full
+
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 1000 full
