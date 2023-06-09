3c72301d3350d69d4a5dec6efc1b3be2bfdd8811
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-06-09 19:24:17 +0200

    Added all experiments to bash script.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,32 +1,65 @@
 #!/bin/bash
 
+##### WORDNET
 
-# company left 
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-yadl.parquet \
+# company left first
+python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation first wordnet_big
-# companies left dedup
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-yadl.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dedup wordnet_big
+# companies left mean
+python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation mean wordnet_big
 # companies left dfs
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-yadl.parquet \
+python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dfs wordnet_big
 
-# # us accidents left noagg
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/us-accidents/us-accidents-yadl.parquet \
+# # us accidents left first
+python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation first wordnet_big
-# us accidents left dedup
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/us-accidents/us-accidents-yadl.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation dedup wordnet_big
+# us accidents left mean
+python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation mean wordnet_big
 # us accidents left dfs
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/us-accidents/us-accidents-yadl.parquet \
+python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation dfs wordnet_big
 
-# presidential left no agg
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/us-presidential-results-yadl.parquet \
+# presidential left first
+python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation first wordnet_big
-# presidential left dedup
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/us-presidential-results-yadl.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dedup wordnet_big
+# presidential left mean
+python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation mean wordnet_big
 # presidential left dfs
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/us-presidential-results-yadl.parquet \
+python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dfs wordnet_big
+
+##### BINARY
+
+# company left first
+python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation first binary
+# companies left mean
+python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation mean binary
+# companies left dfs
+python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dfs binary
+
+# # us accidents left first
+python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation first binary
+# us accidents left mean
+python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation mean binary
+# us accidents left dfs
+python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation dfs binary
+
+# presidential left first
+python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation first binary
+# presidential left mean
+python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation mean binary
+# presidential left dfs
+python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dfs binary
