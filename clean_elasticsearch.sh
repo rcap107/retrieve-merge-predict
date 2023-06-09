62deb2cf68baa9eedcd95c03ccac07b94b7c0a5b
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-06-09 16:42:40 +0200

    Cleaned up root folder a bit.

--- clean_elasticsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Dropping Lazo index"
-
-curl -X GET "localhost:9250/_cat/indices/lazo*?v=true&s=index&pretty"
-
-curl -X DELETE "localhost:9250/lazo?pretty"
-
-curl -X GET "localhost:9250/_cat/indices/lazo*?v=true&s=index&pretty"
-
