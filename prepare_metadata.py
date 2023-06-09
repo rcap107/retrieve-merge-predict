7da4c660fba1eedd0f862f7310f0410cb252b6be
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-06-09 19:24:50 +0200

    Refactored to have a cleaner interface.

--- prepare_metadata.py
+++ prepare_metadata.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from pathlib import Path
+import argparse
 
 import pandas as pd
 import polars as pl
@@ -26,99 +27,127 @@ logger.addHandler(fh)
 logger.addHandler(sh)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-s",
+        "--save_indices",
+        action="store_true",
+        help="If true, save the indices after preparing the metadata.",
+    )
+    parser.add_argument(
+        "case",
+        action="store",
+        metavar="CASE",
+        help="Tag to be assigned to the current index.",
+    )
+    parser.add_argument(
+        "data_folder",
+        action="store",
+        metavar="PATH",
+        help="Path to the folder storing the tables to index.",
+    )
+    parser.add_argument(
+        "--save_to_full",
+        action="store_true",
+        help="Save the current set of mdata to the full index. ",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
 def save_single_table(dataset_path, dataset_source, metadata_dest):
     ds = RawDataset(dataset_path, dataset_source, metadata_dest)
     ds.save_metadata_to_json()
 
 
-def prepare_yadl_versions(cases=[], save_to_full=False):
-    for case in cases:
-        logger.info("Case %s", case)
-        data_folder = Path(f"data/yago3-dl/{case}")
-        if data_folder.exists():
-            os.makedirs(f"data/metadata/{case}", exist_ok=True)
-            for dataset_path in data_folder.glob("**/*.parquet"):
-                ds = RawDataset(dataset_path, "yago3-dl", f"data/metadata/{case}")
-                ds.save_metadata_to_json(f"data/metadata/{case}")
-                if save_to_full:
-                    ds.save_metadata_to_json("data/metadata/full")
-        else:
-            raise FileNotFoundError(f"Invalid path {data_folder}")
-
-    # for case in cases:
-    #     metadata_index = MetadataIndex(f"data/metadata/{case}")
-    #     metadata_index.save_index(f"data/metadata/_mdi/md_index_{case}.pickle")
-
-
-def prepare_gittables():
-    case = "gittables"
+def prepare_metadata_from_case(case, data_folder, save_to_full=False):
     logger.info("Case %s", case)
-    data_folder = Path("data/gittables/extracted/")
+    data_folder = Path(data_folder)
     if data_folder.exists():
-        total = sum(1 for _ in data_folder.glob("**/*.parquet"))
-        for dataset_path in tqdm(data_folder.glob("**/*.parquet"), total=total):
-            ds = RawDataset(dataset_path, "gittables", f"data/metadata/{case}")
+        os.makedirs(f"data/metadata/{case}", exist_ok=True)
+        for dataset_path in data_folder.glob("**/*.parquet"):
+            ds = RawDataset(dataset_path, "yadl", f"data/metadata/{case}")
             ds.save_metadata_to_json(f"data/metadata/{case}")
-            # ds.save_metadata_to_json("data/metadata/full")
-    else:
-        raise FileNotFoundError(f"Invalid path {data_folder}")
-
-def prepare_auctus():
-    case = "auctus"
-    logger.info("Case %s", case)
-    data_folder = Path("data/auctus/parquet/")
-    if data_folder.exists():
-        total = sum(1 for _ in data_folder.glob("**/*.parquet"))
-        for dataset_path in tqdm(data_folder.glob("**/*.parquet"), total=total):
-            ds = RawDataset(dataset_path, "auctus", f"data/metadata/{case}")
-            ds.save_metadata_to_json(f"data/metadata/{case}")
-            # ds.save_metadata_to_json("data/metadata/full")
-    else:
-        raise FileNotFoundError(f"Invalid path {data_folder}")
-
-
-def prepare_indices(cases=[], additional_configurations=None):
-    
-    for case in cases:
+            if save_to_full:
+                ds.save_metadata_to_json("data/metadata/full")
         metadata_index = MetadataIndex(f"data/metadata/{case}")
         metadata_index.save_index(f"data/metadata/_mdi/md_index_{case}.pickle")
+    else:
+        raise FileNotFoundError(f"Invalid path {data_folder}")
 
-    # Prepare indices
-    selected_indices = ["minhash"]
 
+# def prepare_yadl_versions(cases=[], save_to_full=False):
+#     for case in cases:
+#         logger.info("Case %s", case)
+#         data_folder = Path(f"data/yadl/{case}")
+#         if data_folder.exists():
+#             os.makedirs(f"data/metadata/{case}", exist_ok=True)
+#             for dataset_path in data_folder.glob("**/*.parquet"):
+#                 ds = RawDataset(dataset_path, "yadl", f"data/metadata/{case}")
+#                 ds.save_metadata_to_json(f"data/metadata/{case}")
+#                 if save_to_full:
+#                     ds.save_metadata_to_json("data/metadata/full")
+#         else:
+#             raise FileNotFoundError(f"Invalid path {data_folder}")
+
+
+# def prepare_gittables():
+#     case = "gittables"
+#     logger.info("Case %s", case)
+#     data_folder = Path("data/gittables/extracted/")
+#     if data_folder.exists():
+#         total = sum(1 for _ in data_folder.glob("**/*.parquet"))
+#         for dataset_path in tqdm(data_folder.glob("**/*.parquet"), total=total):
+#             ds = RawDataset(dataset_path, "gittables", f"data/metadata/{case}")
+#             ds.save_metadata_to_json(f"data/metadata/{case}")
+#             # ds.save_metadata_to_json("data/metadata/full")
+#     else:
+#         raise FileNotFoundError(f"Invalid path {data_folder}")
+
+# def prepare_auctus():
+#     case = "auctus"
+#     logger.info("Case %s", case)
+#     data_folder = Path("data/auctus/parquet/")
+#     if data_folder.exists():
+#         total = sum(1 for _ in data_folder.glob("**/*.parquet"))
+#         for dataset_path in tqdm(data_folder.glob("**/*.parquet"), total=total):
+#             ds = RawDataset(dataset_path, "auctus", f"data/metadata/{case}")
+#             ds.save_metadata_to_json(f"data/metadata/{case}")
+#             # ds.save_metadata_to_json("data/metadata/full")
+#     else:
+#         raise FileNotFoundError(f"Invalid path {data_folder}")
+
+
+def prepare_indices(case, selected_indices=["minhash"]):
     logger.info("Preparing minhash indices")
-    # for case in ["wordnet"]:
-    for case in cases:
-        index_dir = Path(f"data/metadata/_indices/{case}")
-        logger.info("Minhash: start %s", case)
-        metadata_dir = Path(f"data/metadata/{case}")
-        case_dir = Path(index_dir, case)
-        os.makedirs(case_dir, exist_ok=True)
+    index_dir = Path(f"data/metadata/_indices/{case}")
+    logger.info("Minhash: start %s", case)
+    metadata_dir = Path(f"data/metadata/{case}")
+    case_dir = Path(index_dir, case)
+    os.makedirs(case_dir, exist_ok=True)
 
-        index_configurations = utils.prepare_default_configs(
-            metadata_dir, selected_indices
-        )
-        print("Preparing indices.")
-        indices = utils.prepare_indices(index_configurations)
-        print("Saving indices.")
-        utils.save_indices(indices, index_dir)
-        logger.info("Minhash: end %s", case)
-    logger.info("Done")
+    index_configurations = utils.prepare_default_configs(
+        metadata_dir, selected_indices
+    )
+    print("Preparing indices.")
+    indices = utils.prepare_indices(index_configurations)
+    print("Saving indices.")
+    utils.save_indices(indices, index_dir)
+    logger.info("Minhash: end %s", case)
 
 
-logger.info("Starting metadata creation - YADL")
-prepare_yadl_versions(cases=["wordnet_big"], save_to_full=False)
-logger.debug("Done with metadata - YADL")
+if __name__ == "__main__":
+    args = parse_args()
+    logger.info("START - Metadata creation - %s" % args.case)
 
-# logger.info("Starting metadata creation - gittables")
-# prepare_gittables()
-# logger.debug("Done with metadata - gittables")
+    prepare_metadata_from_case(args.case, args.data_folder, args.save_to_full)
+    logger.info("END - Metadata creation - %s" % args.case)
 
-# logger.info("Starting metadata creation - auctus")
-# prepare_auctus()
-# logger.debug("Done with metadata - auctus")
-
-
-# logger.info("Preparing indices")
-prepare_indices(cases=["wordnet_big"])
-# logger.info("Done with indices")
+    if args.save_indices:
+        logger.info("START - Index creation")
+        prepare_indices(args.case)
+        logger.info("END - Preparing indices")
