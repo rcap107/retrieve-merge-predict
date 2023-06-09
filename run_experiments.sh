afd0b6c995dc94f1f5efe7c3738871b192db2968
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-06-09 16:43:24 +0200

    Updated script for experiments.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
-# us presidential nojoin
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy nojoin wordnet_big
-# companies nojoin
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy nojoin wordnet_big
-# movies nojoin
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy nojoin wordnet_big
+
+
+# company left 
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation first wordnet_big
+# companies left dedup
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dedup wordnet_big
+# companies left dfs
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dfs wordnet_big
+
+# # us accidents left noagg
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/us-accidents/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation first wordnet_big
+# us accidents left dedup
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/us-accidents/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation dedup wordnet_big
+# us accidents left dfs
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/us-accidents/us-accidents-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50  --join_strategy left --aggregation dfs wordnet_big
 
 # presidential left no agg
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left wordnet_big
-# company left no agg
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left wordnet_big
-# movies left no agg
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left wordnet_big
-
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation first wordnet_big
 # presidential left dedup
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left --aggregation dedup wordnet_big
-# companies left dedup
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left --aggregation dedup wordnet_big
-# movies left dedup
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left --aggregation dedup wordnet_big
-
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dedup wordnet_big
 # presidential left dfs
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/presidential-results-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left --aggregation dfs wordnet_big
-# companies left dfs
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/company-employees/company-employees-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left --aggregation dfs wordnet_big
-# movies left dfs
-python main_pipeline.py --source_table_path data/source_tables/ken_datasets/the-movies-dataset/movies-prepared.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 750 --top_k 100  --join_strategy left --aggregation dfs wordnet_big
+python main_pipeline.py --source_table_path data/source_tables/ken_datasets/presidential-results/us-presidential-results-yadl.parquet \
+--query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50  --join_strategy left --aggregation dfs wordnet_big
