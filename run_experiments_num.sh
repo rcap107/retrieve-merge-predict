35d49b6167fdd9e77d35da3c08e2e4abc0a48289
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-07-07 15:49:15 +0200

    Reverting changes

--- run_experiments_num.sh
+++ run_experiments_num.sh
@@ -2,19 +2,29 @@
 
 ##### WORDNET
 
-# company left first
-python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl-ax.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation first wordnet_big_num_cp
-# companies left mean
-python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl-ax.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation mean wordnet_big_num_cp
-# companies left dfs
-python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl-ax.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation dfs wordnet_big_num_cp
+# # movies left first
+# python main_pipeline.py --source_table_path data/source_tables/movies-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation first wordnet_big_num_cp
+# # movies left mean
+# python main_pipeline.py --source_table_path data/source_tables/movies-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation mean wordnet_big_num_cp
+# # movies left dfs
+# python main_pipeline.py --source_table_path data/source_tables/movies-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation dfs wordnet_big_num_cp
 
-# # us accidents left first
-python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl-ax.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50 --n_jobs 8  --join_strategy left --aggregation first wordnet_big_num_cp
+# # company left first
+# python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation first wordnet_big_num_cp
+# # companies left mean
+# python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation mean wordnet_big_num_cp
+# # companies left dfs
+# python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation dfs wordnet_big_num_cp
+
+# # # us accidents left first
+# python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl-ax.parquet \
+# --query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50 --n_jobs 8  --join_strategy left --aggregation first wordnet_big_num_cp
 # us accidents left mean
 python main_pipeline.py --source_table_path data/source_tables/us-accidents-yadl-ax.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500 --top_k 50 --n_jobs 8  --join_strategy left --aggregation mean wordnet_big_num_cp
@@ -31,5 +41,3 @@ python main_pipeline.py --source_table_path data/source_tables/us-presidential-r
 # presidential left dfs
 python main_pipeline.py --source_table_path data/source_tables/us-presidential-results-yadl-ax.parquet \
 --query_column col_to_embed --sampling_seed 42 --iterations 500  --top_k 50 --n_jobs 8  --join_strategy left --aggregation dfs wordnet_big_num_cp
-
-##### BINARY
