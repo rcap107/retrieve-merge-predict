d547fae0a7b35637c38806fabf381e4ffc5abdcc
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-08-08 17:24:07 +0200

    Fixed example config.

--- example_config.sh
+++ example_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python main_pipeline.py --source_table_path data/source_tables/company-employees-yadl.parquet \
---query_column col_to_embed --sampling_seed 42 --iterations 100  --top_k 20  --join_strategy left --n_jobs 1 --aggregation mean wordnet_big_num_cp
+--query_column col_to_embed --sampling_seed 42 --iterations 100  --top_k 20  --join_strategy left  --aggregation mean --selected_indices minhash  --n_jobs 1 wordnet_big_num_cp
