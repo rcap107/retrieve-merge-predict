35b3fa0544a9e2abf1c35486bc4ed74c5b7e66c9
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-09-19 10:54:26 +0200

    Fixed typing bug again, more logging.

--- src/methods/evaluation.py
+++ src/methods/evaluation.py
@@ -23,8 +23,12 @@ model_folder = Path("data/models")
 os.makedirs(model_folder, exist_ok=True)
 
 
-def prepare_table_for_evaluation(df):
-    df = df.fill_nan("null").fill_null("null")
+def prepare_table_for_evaluation(src_df):
+
+    df = src_df.with_columns(
+        src_df.with_columns(cs.string().fill_null("null"), cs.float().fill_null(np.nan))
+    )
+    # df = src_df.fill_null()
     df = utils.cast_features(df)
     return df
 
@@ -34,10 +38,10 @@ def prepare_X_y(
     target_column,
 ):
     y = src_df[target_column].to_pandas()
-    df = src_df.drop(target_column)
+    df = src_df.drop(target_column).fill_null(value="null").fill_nan(value=np.nan)
     df = utils.cast_features(df)
     cat_features = df.select(cs.string()).columns
-    X = df.fill_null(value="null").fill_nan(value=np.nan).to_pandas()
+    X = df.to_pandas()
 
     return X, y, cat_features
 
@@ -98,15 +102,13 @@ def run_on_base_table(
         raw_logger.set_run_status("SUCCESS")
         raw_logger.end_time("run")
         run_logger.end_time("run")
-        logger_sh.info(
-            "Fold %d: Base table R2 %.4f" % (idx + 1, raw_logger.results["r2score"])
-        )
-
         raw_logger.to_raw_log_file()
 
     run_logger.results["avg_r2"] = np.mean(r2_results)
     run_logger.results["std_r2"] = np.std(r2_results)
     run_logger.to_run_log_file()
+
+    logger_sh.info("Base table R2 %.4f" % (run_logger.results["avg_r2"]))
 
     results = {
         "index": "base_table",
@@ -261,6 +263,7 @@ def run_on_candidates(
             raw_logger.set_run_status("SUCCESS")
             raw_logger.end_time("run")
             run_logger.end_time("run")
+            raw_logger.to_raw_log_file()
 
         avg_r2 = np.mean(dict_r2_by_cand[hash_])
         avg_r2_by_cand[hash_] = avg_r2
