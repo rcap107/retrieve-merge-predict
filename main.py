6a08a488cb03fe1dfbeffd7c0a1c16f930f3a9bf
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-09-20 14:44:03 +0200

    Adding copy of config to exp

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import argparse
 import itertools
-from types import SimpleNamespace
-import pprint
-import toml
 import logging
-from main_pipeline import single_run
 import os
+import pprint
+from types import SimpleNamespace
 
+import toml
+
+from main_pipeline import single_run
 from src.data_structures.loggers import setup_run_logging
 
 logger_sh = logging.getLogger("pipeline")
@@ -55,11 +56,9 @@ if __name__ == "__main__":
     os.makedirs("results/logs", exist_ok=True)
     os.makedirs("results/json", exist_ok=True)
 
-    run_name = setup_run_logging()
-
     base_config = toml.load(args.input_path)
-
     run_variants = generate_run_variants(base_config)
+    exp_name = setup_run_logging(base_config)
 
     for idx, dd in enumerate(run_variants):
         print("#" * 80)
@@ -67,4 +66,4 @@ if __name__ == "__main__":
         print("#" * 80)
         pprint.pprint(dd)
         ns = SimpleNamespace(**dd)
-        single_run(ns, run_name)
+        single_run(ns, exp_name)
