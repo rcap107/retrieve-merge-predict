62ccb549be34302cc0f04091ec063aae5f6228e8
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-10-09 12:10:47 +0200

    More hardcoded crap

--- main_pipeline.py
+++ main_pipeline.py
@@ -263,15 +263,17 @@ def single_run(args, run_name=None):
             target_column="target",
             chosen_model=args.chosen_model,
             join_estimators=[
-                "no_join",
-                "highest_containment",
-                "best_single_join",
-                "full_join",
+                # "no_join",
+                # "highest_containment",
+                # "best_single_join",
+                # "full_join",
                 "stepwise_greedy_join",
             ],
             join_parameters=join_parameters,
             model_parameters=model_parameters,
             n_splits=args.n_splits,
+            greedy_iterations=args.greedy_iterations,
+            epsilon=args.epsilon,
         )
         logger.info("End evaluation.")
         scl.add_timestamp("end_evaluation")
