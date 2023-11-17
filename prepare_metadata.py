f14e8ca6291d2b0e17b3a0daaa52998ef5d7bb3a
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-11-17 16:21:30 +0100

    Cleaning up preparation code.

--- prepare_metadata.py
+++ prepare_metadata.py
@@ -1,6 +1,4 @@
 import argparse
-import datetime as dt
-import logging
 import os
 from pathlib import Path
 from types import SimpleNamespace
@@ -8,17 +6,25 @@ from types import SimpleNamespace
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
-import src.pipeline as pipeline
-from src.data_structures.metadata import MetadataIndex, RawDataset
+from src.data_structures.metadata import MetadataIndex
 from src.utils.indexing import save_single_table
 
 
-def prepare_metadata_from_case(case, data_folder, save_to_full=False):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_folder", action="store")
+
+    args = parser.parse_args()
+    return args
+
+
+def prepare_metadata_from_case(data_folder):
     # logger.info("Case %s", case)
     data_folder = Path(data_folder)
+    case = data_folder.stem
     if data_folder.exists():
         os.makedirs(f"data/metadata/{case}", exist_ok=True)
-        os.makedirs(f"data/metadata/_mdi", exist_ok=True)
+        os.makedirs("data/metadata/_mdi", exist_ok=True)
 
         total_files = sum(1 for f in data_folder.glob("**/*.parquet"))
 
@@ -38,15 +44,11 @@ def prepare_metadata_from_case(case, data_folder, save_to_full=False):
 
 
 if __name__ == "__main__":
-    # args = parse_args()
-    a = {
-        "case": "binary_update",
-        "data_folder": "data/yadl/binary_update",
-        "save_to_full": False,
-    }
+    args = parse_args()
+    # a = {
+    #     "case": "binary_update",
+    #     "data_folder": "data/yadl/binary_update",
+    # }
 
-    args = SimpleNamespace(**a)
-    start_time = dt.datetime.now()
-    # logger.info("START - Metadata creation - %s" % args.case)
-    prepare_metadata_from_case(args.case, args.data_folder, args.save_to_full)
-    # logger.info("END - Metadata creation - %s" % args.case)
+    # args = SimpleNamespace(**a)
+    prepare_metadata_from_case(args.data_folder)
--- query_indices.py
+++ query_indices.py
@@ -8,16 +8,15 @@ from src.data_structures.metadata import MetadataIndex
 from src.utils.indexing import DEFAULT_INDEX_DIR, load_index, query_index
 
 # %%
-config = toml.load("config/join_discovery/query_exact_matching_binary.toml")
+config = toml.load("config/join_discovery/query-wordnet_big.toml")
 
-jd_method = config["join_discovery_method"]
+jd_methods = config["join_discovery_method"]
 data_lake_version = config["data_lake"]
 
 query_cases = config["query_cases"]
 
 metadata_dir = Path(f"data/metadata/{data_lake_version}")
 metadata_index_path = Path(f"data/metadata/_mdi/md_index_{data_lake_version}.pickle")
-index_dir = Path(f"data/metadata/_indices/{data_lake_version}")
 # %%
 if not metadata_index_path.exists():
     raise FileNotFoundError(f"Path to metadata index {metadata_index_path} is invalid.")
@@ -25,27 +24,28 @@ mdata_index = MetadataIndex(
     data_lake_variant=data_lake_version, index_path=metadata_index_path
 )
 # %%
-if jd_method != "exact_matching":
-    print("Loading index...")
-    index = load_index(config)
-    print("Querying...")
-    for query_case in query_cases:
-        query_tab_path = Path(query_case["table_path"])
-        query_column = query_case["query_column"]
-        query_index(index, query_tab_path, query_column, mdata_index)
-else:
-    for query_case in query_cases:
-        tname = Path(query_case["table_path"]).stem
-        query_tab_path = Path(query_case["table_path"])
-        query_column = query_case["query_column"]
-        index_path = Path(
-            DEFAULT_INDEX_DIR,
-            data_lake_version,
-            f"cv_index_{tname}_{query_column}.pickle",
-        )
+if "minhash" in jd_methods:
+    minhash_index = load_index(
+        {"join_discovery_method": "minhash", "data_lake": data_lake_version}
+    )
 
-        index = ExactMatchingIndex(file_path=index_path)
+# %%
+for query_case in query_cases:
+    tname = Path(query_case["table_path"]).stem
+    query_tab_path = Path(query_case["table_path"])
+    query_column = query_case["query_column"]
+    for jd_method in jd_methods:
+        if jd_method == "minhash":
+            index = minhash_index
+        elif jd_method == "exact_matching":
+            index_path = Path(
+                DEFAULT_INDEX_DIR,
+                data_lake_version,
+                f"em_index_{tname}_{query_column}.pickle",
+            )
+            index = ExactMatchingIndex(file_path=index_path)
+        else:
+            raise ValueError
         query_index(index, query_tab_path, query_column, mdata_index)
-
 
 # %%
