5e36e2da35752a6a1d843fb88208c0b8c6370554
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-11-27 15:35:08 +0100

    Updated code to use updated MetadataIndex

--- prepare_metadata.py
+++ prepare_metadata.py
@@ -11,41 +11,17 @@ from src.data_structures.metadata import MetadataIndex
 from src.utils.indexing import save_single_table
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("data_folder", action="store")
-
-    args = parser.parse_args()
-    return args
-
-
 def prepare_metadata_from_case(data_folder):
     # logger.info("Case %s", case)
     data_folder = Path(data_folder)
-    case = data_folder.stem
     if data_folder.exists():
-        os.makedirs(f"data/metadata/{case}", exist_ok=True)
-        os.makedirs("data/metadata/_mdi", exist_ok=True)
-
-        total_files = sum(1 for f in data_folder.glob("**/*.parquet"))
-
-        metadata_dest = f"data/metadata/{case}"
-        Parallel(n_jobs=-1, verbose=0)(
-            delayed(save_single_table)(dataset_path, "yadl", metadata_dest)
-            for dataset_path in tqdm(
-                data_folder.glob("**/*.parquet"), total=total_files
-            )
-        )
-        metadata_index = MetadataIndex(
-            data_lake_variant=case, metadata_dir=f"data/metadata/{case}"
-        )
-        metadata_index.save_index(f"data/metadata/_mdi/md_index_{case}.pickle")
+        metadata_index = MetadataIndex(data_lake_path=data_folder, n_jobs=1)
+        metadata_index.create_index()
     else:
         raise FileNotFoundError(f"Invalid path {data_folder}")
 
 
 if __name__ == "__main__":
-    args = parse_args()
     a = {
         "case": "binary_update",
         "data_folder": "data/yadl/binary_update",
