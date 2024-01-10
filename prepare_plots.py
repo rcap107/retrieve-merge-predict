a3deebdc6c0fb357563900e684289ff586608360
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-01-10 17:48:34 +0100

    ♻️ More scripting

--- prepare_plots.py
+++ prepare_plots.py
@@ -16,13 +16,10 @@ import seaborn as sns
 from matplotlib.gridspec import GridSpec
 
 import src.utils.plotting as plotting
-from src.utils.logging import read_logs
+from src.utils.logging import read_and_process, read_logs
 
 cfg = pl.Config()
 cfg.set_fmt_str_lengths(150)
-
-# %%
-# Prepare data for the three plot cases
 
 # %%
 root_path = Path("results/big_batch")
@@ -33,164 +30,19 @@ for rpath in root_path.iterdir():
 
 df_results = pl.concat(df_list)
 
-# %%
-df_ = df_results.select(
-    pl.col(
-        [
-            "scenario_id",
-            "target_dl",
-            "jd_method",
-            "base_table",
-            "estimator",
-            "chosen_model",
-            "aggregation",
-            "r2score",
-            "time_fit",
-            "time_predict",
-            "time_run",
-            "epsilon",
-        ]
-    )
-).filter(
-    (~pl.col("base_table").str.contains("open_data"))
-    & (pl.col("target_dl") != "wordnet_big")
-)
-df_ = df_.group_by(
-    ["target_dl", "jd_method", "base_table", "estimator", "chosen_model"]
-).map_groups(lambda x: x.with_row_count("fold_id"))
-
-# %%
-joined = df_.join(
-    df_.filter(pl.col("estimator") == "nojoin"),
-    on=["target_dl", "jd_method", "base_table", "chosen_model", "fold_id"],
-    how="left",
-).with_columns((pl.col("r2score") - pl.col("r2score_right")).alias("difference"))
-
-# %%
-results_full = joined.filter(~pl.col("base_table").str.contains("depleted"))
-results_depleted = joined.filter(pl.col("base_table").str.contains("depleted"))
-
-results_full = prepare_data_for_plotting(results_full)
-results_depleted = prepare_data_for_plotting(results_depleted)
-
-# %%
-current_results = results_depleted.clone()
-projection = [
-    "fold_id",
-    "target_dl",
-    "jd_method",
-    "base_table",
-    "estimator",
-    "chosen_model",
-    "aggregation",
-    "r2score",
-    "time_fit",
-    "time_predict",
-    "time_run",
-    "difference",
-    "scaled_diff",
-]
-current_results = current_results.select(projection)
-# %%
-current_results = current_results.filter(pl.col("estimator") != "nojoin")
-# %%
-p = get_difference_from_mean(
-    current_results, column_to_average="chosen_model", result_column="r2score"
-)
-
-# %%
-# Prepare comparison figure
-def prepare_data_for_comparison(df, variable_of_interest):
-    df = get_difference_from_mean(
-        df, column_to_average=variable_of_interest, result_column="r2score"
-    )
-    df = get_difference_from_mean(
-        df,
-        column_to_average=variable_of_interest,
-        result_column="time_run",
-        geometric=True,
-    )
-
-    return df
-
-
-# %%
-df_tri = prepare_data_for_comparison(current_results, "jd_method")
-# %%
-plotting.draw_triple_comparison(
-    df_tri,
-    "estimator",
-    scatterplot_dimension="chosen_model",
-    figsize=(12, 4),
-    scatter_mode="split",
-    savefig=True,
-)
-
-# %%
-cases = get_cases(
-    current_results,
-)
-
-# # %%
-# for outer_variable in cases:
-#     plotting.draw_split_figure(cases, outer_variable, current_results)
-
-# %%
-plotting.draw_split_figure(
-    cases,
-    split_dimension="chosen_model",
-    df=current_results,
-    grouping_dimensions=["estimator"],
-    kind="box",
-    scatterplot_dimension="base_table",
-    colormap_name="viridis",
-    plotting_variable="scaled_diff",
-)
-
-# %%
-target_variable = "estimator"
-result_column = "scaled_diff"
-_prep = get_difference_from_mean(
-    current_results, column_to_average=target_variable, result_column=result_column
-)
-
-plotting.draw_split_figure(
-    cases,
-    split_dimension="chosen_model",
-    df=_prep,
-    grouping_dimensions=[target_variable],
-    scatterplot_dimension="base_table",
-    xtick_format="percentage",
-    colormap_name="viridis",
-    plotting_variable=f"diff_from_mean_{result_column}",
-    # plot_label=f"Difference from mean {target_variable}",
-    kind="box",
-    figsize=(8, 3),
-)
-
 #%%
+results_full, results_depleted = read_and_process(df_results)
 
-# Create a figure and a GridSpec with 2 rows and 2 columns
-fig = plt.figure(figsize=(8, 3))
-gs = GridSpec(2, 2, width_ratios=[2, 1])
 
-# Add subplots to the GridSpec
-ax_left = fig.add_subplot(gs[:, 0])  # One plot in the left column, spanning both rows
-ax_top_right = fig.add_subplot(gs[0, 1])  # Top plot in the right column
-ax_bottom_right = fig.add_subplot(
-    gs[1, 1], sharey=ax_top_right
-)  # Bottom plot in the right column
+# %%
+case = "dep"
 
-# Customize the subplots (you can plot your data here)
-ax_left.set_title("Left Plot")
-ax_top_right.set_title("Top Right Plot")
-ax_bottom_right.set_title("Bottom Right Plot")
+if case == "dep":
+    current_results = results_depleted.clone()
+    current_results = current_results.filter(pl.col("estimator") != "nojoin")
 
-# Adjust layout to prevent clipping of titles
-plt.tight_layout()
-
-# Show the plot
-plt.show()
+elif case == "full":
+    current_results = results_full.clone()
 
 # %%
 import matplotlib.pyplot as plt
@@ -204,16 +56,25 @@ fig, axes = plt.subplot_mosaic(
     width_ratios=(3, 1),
 )
 axes[3].set_frame_on(False)
+axes[3].spines["top"].set_visible(False)
+axes[3].spines["right"].set_visible(False)
+axes[3].set_xticks([])
+axes[3].set_yticks([])
 
-
-def simpleaxis(ax):
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    return ax
-
-
-axes[3] = simpleaxis(axes[3])
+# %%
+var = "estimator"
+scatter_d = "base_table"
+plotting.draw_pair_comparison(
+    current_results,
+    var,
+    form_factor="multi",
+    scatterplot_dimension=scatter_d,
+    figsize=(10, 3),
+    scatter_mode="split",
+    savefig=False,
+    savefig_type=["png", "pdf"],
+    case=case,
+    colormap_name="Set1",
+)
 
 # %%
