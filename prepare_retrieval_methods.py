b6de075fab61d11e9f55ecd5f8e409d0351b10f2
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-01-29 13:55:13 +0100

    🎨 Add iterations for tracking retrieval time

--- prepare_retrieval_methods.py
+++ prepare_retrieval_methods.py
@@ -13,6 +13,7 @@ from src.utils.indexing import prepare_join_discovery_methods
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("config_file", action="store")
+    parser.add_argument("--repeats", action="store", type=int, default=1)
 
     args = parser.parse_args()
     return args
@@ -23,6 +24,7 @@ if __name__ == "__main__":
     config_file_path = args.config_file
     if Path(config_file_path).exists():
         config = toml.load(config_file_path)
-        prepare_join_discovery_methods(config)
+        for _ in range(args.repeats):
+            prepare_join_discovery_methods(config)
     else:
         raise FileNotFoundError
