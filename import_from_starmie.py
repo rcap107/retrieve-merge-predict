eedebe85bb6780b34a39046f8f5e0be8257a8433
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-04-22 17:47:30 +0200

    🎨 Reformatting

--- import_from_starmie.py
+++ import_from_starmie.py
@@ -8,11 +8,11 @@ from src.data_structures.retrieval_methods import StarmieWrapper
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "base_path", type=str, help="Path to STARMIE dir containing the query results."
+        "base_path", type=Path, help="Path to STARMIE dir containing the query results."
     )
-    parser.add_argument(
-        "data_lake_version", type=str, help="Data lake version to evaluate."
-    )
+    # parser.add_argument(
+    #     "data_lake_version", type=str, help="Data lake version to evaluate."
+    # )
 
     return parser.parse_args()
 
@@ -20,14 +20,16 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    data_lake_version = args.data_lake_version
+    # data_lake_version = args.data_lake_version
     root_path = args.base_path
+    data_lake_version = root_path.stem
+    # base_path = Path(root_path, data_lake_version)
 
-    base_path = Path(root_path, data_lake_version)
+    print(root_path)
 
-    for pth in base_path.glob("*.parquet"):
+    for pth in root_path.glob("*.parquet"):
         print(pth)
-        base_table_path = Path(pth.stem.replace("query-results_cl_", "")).with_suffix(
+        base_table_path = Path(pth.stem.replace("starmie-cl_", "")).with_suffix(
             ".parquet"
         )
         output_dir = Path(f"data/metadata/_indices/{data_lake_version}")
