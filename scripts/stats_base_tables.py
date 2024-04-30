ee2869dc72cc6e2c65775d59a0c7f4ebfdc6fcb6
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-04-30 13:48:39 +0200

    🎨 Adding polars config

--- scripts/stats_base_tables.py
+++ scripts/stats_base_tables.py
@@ -6,6 +6,9 @@ from pathlib import Path
 import polars as pl
 import polars.selectors as cs
 
+cfg = pl.Config()
+cfg.set_fmt_str_lengths(150)
+
 #%%
 list_d = []
 for pth in Path("data/source_tables/yadl").iterdir():
@@ -16,6 +19,7 @@ for pth in Path("data/source_tables/yadl").iterdir():
     list_d.append(d)
 # %%
 df_stats = pl.from_dicts(list_d)
+# %%
 df_stats.write_csv("stats_tables_yadl.csv")
 # %%
 list_d = []
