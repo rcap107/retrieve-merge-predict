d2fcc301f636276285081026ce51b49eb6c2dfe7
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-05-13 15:24:47 +0200

    🎨 Final update plot retrieval

--- debug_plotting.py
+++ debug_plotting.py
@@ -1,22 +1,15 @@
 # %%
 # %cd ~/bench
-# #%%
 # %load_ext autoreload
 # %autoreload 2
 # %%
 from pathlib import Path
 
-import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import polars as pl
-import polars.selectors as cs
-import seaborn as sns
-from matplotlib.gridspec import GridSpec
 
 import src.utils.plotting as plotting
-from src.utils.logging import read_and_process, read_logs
+from src.utils.logging import read_and_process
 
 cfg = pl.Config()
 cfg.set_fmt_str_lengths(150)
@@ -26,37 +19,16 @@ result_path = "results/overall/overall_first.parquet"
 
 df_results = pl.read_parquet(result_path)
 
-results_full, results_depleted = read_and_process(df_results)
-case = "dep"
-
-if case == "dep":
-    current_results = results_depleted.clone()
-    current_results = current_results.filter(pl.col("estimator") != "nojoin")
-
-elif case == "full":
-    current_results = results_full.clone()
-#%%
+current_results = read_and_process(df_results)
+current_results = current_results.filter(pl.col("estimator") != "nojoin")
+# %%
 _d = current_results.filter(
     (pl.col("estimator") != "top_k_full_join")
     & (pl.col("jd_method") != "starmie")
     # & (~pl.col("target_dl").is_in(["wordnet_vldb_50","open_data_us"]) )
 )
-
-var = "jd_method"
-scatter_d = "case"
-plotting.draw_pair_comparison(
-    _d,
-    var,
-    form_factor="multi",
-    scatterplot_dimension=scatter_d,
-    figsize=(10, 3),
-    scatter_mode="split",
-    savefig=False,
-    savefig_type=["png", "pdf"],
-    case=case,
-    # colormap_name="Set1",
-)
-
+# %%
+plot_case = "dep"
 var = "estimator"
 scatter_d = "case"
 plotting.draw_pair_comparison(
@@ -64,14 +36,17 @@ plotting.draw_pair_comparison(
     var,
     form_factor="multi",
     scatterplot_dimension=scatter_d,
-    figsize=(10, 3),
+    figsize=(10, 2.1),
     scatter_mode="split",
-    savefig=False,
+    savefig=True,
     savefig_type=["png", "pdf"],
-    case=case,
+    case=plot_case,
+    jitter_factor=0.01,
+    qle=0.05,
+    add_titles=True,
     # colormap_name="Set1",
 )
-
+# %%
 var = "chosen_model"
 scatter_d = "case"
 plotting.draw_pair_comparison(
@@ -79,32 +54,13 @@ plotting.draw_pair_comparison(
     var,
     form_factor="multi",
     scatterplot_dimension=scatter_d,
-    figsize=(10, 3),
+    figsize=(10, 1),
     scatter_mode="split",
-    savefig=False,
+    savefig=True,
     savefig_type=["png", "pdf"],
-    case=case,
+    case=plot_case,
+    add_titles=False,
     # colormap_name="Set1",
 )
 
 # %%
-_d = current_results.filter(
-    (pl.col("estimator") == "top_k_full_join")
-    & (pl.col("jd_method") != "starmie")
-    # & (~pl.col("target_dl").is_in(["wordnet_vldb_50","open_data_us"]) )
-)
-#%%
-var = "jd_method"
-scatter_d = "case"
-plotting.draw_pair_comparison(
-    _d,
-    var,
-    form_factor="multi",
-    scatterplot_dimension=scatter_d,
-    figsize=(10, 3),
-    scatter_mode="split",
-    savefig=False,
-    savefig_type=["png", "pdf"],
-    case=case,
-    # colormap_name="Set1",
-)
