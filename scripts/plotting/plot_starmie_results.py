1178e3c9aa8ead567f0eccbede77b85eeb673ace
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-05-13 15:16:36 +0200

    🎨 Final update plot retrieval

--- scripts/plotting/plot_starmie_results.py
+++ scripts/plotting/plot_starmie_results.py
@@ -1,23 +1,15 @@
 # %%
 # %cd ~/bench
-# #%%
+# %%
 # %load_ext autoreload
 # %autoreload 2
 # %%
-from pathlib import Path
 
-import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import polars as pl
-import polars.selectors as cs
-import seaborn as sns
-from matplotlib.gridspec import GridSpec
 
 import src.utils.plotting as plotting
-from src.utils import constants
-from src.utils.logging import read_and_process, read_logs
+from src.utils.logging import read_and_process
 
 cfg = pl.Config()
 cfg.set_fmt_str_lengths(150)
@@ -56,14 +48,14 @@ grouping_dimension = "jd_method"
 scatter_d = "case"
 
 df_rel_r2 = plotting.get_difference_from_mean(
-    _d, column_to_average=grouping_dimension, result_column="r2score"
+    _d, column_to_average=grouping_dimension, result_column="y"
 )
 df_rel_time = plotting.get_difference_from_mean(
     _d, column_to_average=grouping_dimension, result_column="time_run", geometric=True
 )
 
 formatting_dict = {
-    f"diff_{grouping_dimension}_r2score": {"xtick_format": "percentage"},
+    f"diff_{grouping_dimension}_y": {"xtick_format": "percentage"},
     f"diff_{grouping_dimension}_time_run": {"xtick_format": "symlog"},
 }
 
@@ -111,13 +103,67 @@ res_time = prep_difference(_df, "jd_method", "time_retrieval")
 
 
 # %%
-fig, axs = plt.subplots(
-    1, 4, squeeze=True, layout="constrained", figsize=(12, 3), sharey=True
+fig = plt.figure(layout="constrained", figsize=(12, 3))
+subfigs = fig.subfigures(
+    1,
+    2,
+    #  wspace=0.07
 )
 
-var_to_plot = f"diff_{grouping_dimension}_r2score"
+# Left subfig for retrieval (it's the first step)
+subfigs[0].set_facecolor("bisque")
+subfigs[0].suptitle("Retrieval")
+
+# Right subfig for prediction pipeline (it's the second step)
+subfigs[1].set_facecolor("lightcyan")
+subfigs[1].suptitle("Prediction pipeline")
+
+axs_left = subfigs[0].subplots(1, 2, sharey=True)
+axs_right = subfigs[1].subplots(1, 2, sharey=True)
+
+locations = [0.01, 1, 2, 3, 5, 10]
+labels = [
+    r"$0.01x$",
+    r"$1x$",
+    r"$2x$",
+    r"$3x$",
+    r"$5x$",
+    r"$10x$",
+]
+symlog_ticks = [locations, labels]
+
+# Peak RAM
 plotting.prepare_case_subplot(
-    axs[0],
+    axs_left[0],
+    res_mem,
+    "jd_method",
+    plotting_variable="diff_peak_memory",
+    sorting_method="manual",
+    sorting_variable="jd_method",
+    xtick_format="symlog",
+    qle=0.02,
+    symlog_ticks=symlog_ticks,
+)
+# Retrieval time difference
+plotting.prepare_case_subplot(
+    axs_left[1],
+    res_time,
+    "jd_method",
+    plotting_variable="diff_time_retrieval",
+    sorting_method="manual",
+    sorting_variable="jd_method",
+    xtick_format="symlog",
+    qle=0.02,
+    symlog_ticks=symlog_ticks,
+)
+
+axs_left[0].set_title("Peak RAM")
+axs_left[1].set_title("Time difference")
+
+# Prediction performance
+var_to_plot = f"diff_{grouping_dimension}_y"
+plotting.prepare_case_subplot(
+    axs_right[0],
     df=df_rel_r2,
     grouping_dimension=grouping_dimension,
     scatterplot_dimension=scatter_d,
@@ -131,13 +177,14 @@ plotting.prepare_case_subplot(
     xtick_format=formatting_dict[var_to_plot]["xtick_format"],
     scatterplot_mapping=scatterplot_mapping,
 )
-axs[0].set_title(
+axs_right[0].set_title(
     rf"$R^2$ difference",
 )
 
+# Time prediction pipeline
 var_to_plot = f"diff_{grouping_dimension}_time_run"
 plotting.prepare_case_subplot(
-    axs[1],
+    axs_right[1],
     df=df_rel_time,
     grouping_dimension=grouping_dimension,
     scatterplot_dimension=scatter_d,
@@ -151,55 +198,15 @@ plotting.prepare_case_subplot(
     xtick_format=formatting_dict[var_to_plot]["xtick_format"],
     scatterplot_mapping=scatterplot_mapping,
 )
-axs[1].set_title(
+axs_right[1].set_title(
     rf"Time difference",
 )
 
-locations = [0.01, 1, 2, 3, 5, 10]
-labels = [
-    r"$0.01x$",
-    r"$1x$",
-    r"$2x$",
-    r"$3x$",
-    r"$5x$",
-    r"$10x$",
-]
-symlog_ticks = [locations, labels]
-
-plotting.prepare_case_subplot(
-    axs[2],
-    res_mem,
-    "jd_method",
-    plotting_variable="diff_peak_memory",
-    sorting_method="manual",
-    sorting_variable="jd_method",
-    xtick_format="symlog",
-    qle=0.02,
-    symlog_ticks=symlog_ticks,
-)
-
-plotting.prepare_case_subplot(
-    axs[3],
-    res_time,
-    "jd_method",
-    plotting_variable="diff_time_retrieval",
-    sorting_method="manual",
-    sorting_variable="jd_method",
-    xtick_format="symlog",
-    qle=0.02,
-    symlog_ticks=symlog_ticks,
-)
-
-axs[2].set_title("Relative Peak RAM")
-axs[3].set_title("Relative retrieval time")
+# Remove the labels from the right axis
+axs_right[0].yaxis.set_major_locator(plt.NullLocator())
 
 fig.savefig("images/comparison_retrieval_methods.png")
 fig.savefig("images/comparison_retrieval_methods.pdf")
-
-# %%
-fig, axs = plt.subplots(
-    1, 2, squeeze=True, sharey=True, figsize=(10, 2), layout="constrained"
-)
 
 
 # %%
