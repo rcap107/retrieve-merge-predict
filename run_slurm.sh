592723998c6545b1197399ad64cd9085dd129d82
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-14 13:37:13 +0200

    🔧 Added slurm batch files

--- run_slurm.sh
+++ run_slurm.sh
@@ -8,6 +8,12 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=32
 #SBATCH --error error_%A_a.out
+#
+#SBATCH --array=0-3
 
 
-srun python main.py --input_path config/evaluation/general/config-vldb.toml
+CONFIG_RUNS=(config-vldb-yadlbase.toml config-vldb-yadl10k.toml config-vldb-yadl50k.toml config-vldb-binary.toml)
+
+srun python main.py --input_path config/evaluation/general/${CONFIG_RUNS[$SLURM_ARRAY_TASK_ID]}
+
+sleep 5
