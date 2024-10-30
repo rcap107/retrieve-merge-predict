c5f2cd1d782b13dd3a4c18233cf092e2833be846
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-30 10:27:32 +0100

    Removed hardcoded credentials

--- src/utils/notifications.py
+++ src/utils/notifications.py
@@ -3,7 +3,7 @@ import apprise
 
 def get_apobj():
     apobj = apprise.Apprise()
-    with open("../../discord_credentials.txt") as fp:
+    with open("discord_credentials.txt") as fp:
         webhook_path = fp.readline()
     apobj.add(webhook_path)
 
