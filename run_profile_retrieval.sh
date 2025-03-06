be73f877ee8d69c4641e5e4dc57c4037e85b80e7
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-03-06 11:03:38 +0100

    Updating script to use slurm

--- run_profile_retrieval.sh
+++ run_profile_retrieval.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+#
+#SBATCH --job-name=test_job
+#SBATCH --output=res_test_job_%A_%a.txt
+#
+#SBATCH --ntasks=5
+#SBATCH --time=10:00:00          
+#SBATCH --cpus-per-task=32
+#SBATCH --partition=parietal,normal
+#SBATCH --error error_%A_%a.out
+#
+#SBATCH --array=0-5
 
 # Define the lists
 data_lakes=("open_data_us" "wordnet_base" "wordnet_vldb_10" "wordnet_vldb_50" "binary_update")
 retrieval_methods=("exact_matching" "minhash")
 
-# Iterate over each combination of DATA_LAKE and RETRIEVAL_METHOD
-for data_lake in "${data_lakes[@]}"; do
-  for retrieval_method in "${retrieval_methods[@]}"; do
-    echo "Running with DATA_LAKE=${data_lake} and RETRIEVAL_METHOD=${retrieval_method}"
-    
-    # Run the Python script with the current combination
-    python profile_retrieval.py --data_lake_version "$data_lake" --retrieval_method "$retrieval_method" --n_iter 10
-  done
-done
+srun python profile_retrieval.py --data_lake_version ${data_lake[$SLURM_ARRAY_TASK_ID]} --retrieval_method exact_matching --n_iter 3
+sleep 5
+
