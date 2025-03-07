43309fbe259f8c905d3edb1a855f2afe988a1e58
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-03-07 14:29:01 +0100

    Reworking pretty much the entire script

--- scripts/plotting/plot_pareto_comparison_with_querying.py
+++ scripts/plotting/plot_pareto_comparison_with_querying.py
@@ -1,22 +1,21 @@
-#%%
+# %%
 # %cd ~/bench
-#%%
+# %%
 import matplotlib.pyplot as plt
-import numpy as np
 import polars as pl
 import seaborn as sns
-from matplotlib.ticker import FixedLocator, FuncFormatter, NullLocator, AutoLocator
+from matplotlib.ticker import FixedLocator, FuncFormatter, NullLocator
 
 from src.utils import constants, plotting
-from src.utils.logging import read_and_process
+from matplotlib.lines import Line2D
+
 
 sns.set_context("talk")
 plt.style.use("seaborn-v0_8-talk")
 plt.rc("font", family="sans-serif")
 
-
 def major_gigabyte_formatter(x, pos):
-    return f"{x/1e3:.0f}"
+    return f"{x/1e3:.0f}GB"
 
 
 def minor_gigabyte_formatter(x, pos):
@@ -24,11 +23,9 @@ def minor_gigabyte_formatter(x, pos):
 
 
 def major_time_formatter(x, pos):
-    # return f"{x/60:.0f}min"
     if x > 60:
-        return f"{x/60:.0f}min"
-    else:
-        return f"{x:.0f}s"
+        return f"{x/60:.0f}m"
+    return f"{x:.0f}s"
 
 
 # Create a FuncFormatter object
@@ -36,11 +33,14 @@ major_gb_formatter = FuncFormatter(major_gigabyte_formatter)
 minor_gb_formatter = FuncFormatter(minor_gigabyte_formatter)
 
 # Fixed locators
+# Values are tweaked manually for readibility 
 major_time_locator = FixedLocator([10, 120, 600, 3600, 15000])
-gb_locator = FixedLocator([2000, 3000, 4000, 5000, 8000, 10000, 15000, 20000,145_000])
+gb_locator = FixedLocator([2000, 3000, 4000, 5000, 7000, 8500, 12000, 18000, 145_000])
 # gb_locator = AutoLocator()
-#%% RESULTS WITH STARMIE, NO OPEN DATA OR YADL50K
+
+# %% RESULTS WITH STARMIE, NO OPEN DATA OR YADL50K
 df = pl.read_parquet("results/results_retrieval.parquet")
+# Clamping negative results to avoid breaking the scale 
 df = df.with_columns(
     pl.when(pl.col("prediction_metric") < -1)
     .then(-1)
@@ -48,12 +48,13 @@ df = df.with_columns(
     .alias("y")
 ).filter(pl.col("estimator") != "nojoin")
 
-
-query_times_retrieval = pl.read_csv("stats/avg_query_time_for_pareto_plot_retrieval.csv")
+query_times_retrieval = pl.read_csv(
+    "stats/avg_query_time_for_pareto_plot_retrieval.csv"
+)
 query_ram_retrieval = pl.read_csv("stats/dummy_peak_ram.csv")
 # query_times_all_datalakes = pl.read_csv("stats/avg_query_time_for_pareto_plot_all_datalakes.csv")
 
-#%%
+# %%
 # Setting constants
 grouping_variables = {1: "estimator", 2: "chosen_model", 0: "jd_method"}
 titles = {0: "Selector", 1: "Prediction Model", 2: "Retrieval method"}
@@ -77,47 +78,47 @@ palettes = {0: "tab10", 1: "tab10", 2: "tab10"}
 
 keys = ["jd_method", "estimator", "aggregation", "chosen_model"]
 
-#%%
-res = df.group_by(keys).agg(
-    pl.mean("y"),
-    pl.sum("time_run")/15,
-    pl.max("peak_fit"),
-).join(query_times_retrieval, on="jd_method").with_columns(
-    time_run=pl.col("time_run")+pl.col("time_query")).join(
-        query_ram_retrieval, on="jd_method").with_columns(max_ram=pl.max_horizontal("peak_fit", "peak_ram"))
-
-#%%
-fig = plt.figure(figsize=(12, 3))
-
-subfigs = fig.subfigures(1,3)
-
-sfigs[1].subplots_adjust(hspace=0.02)
+res = (
+    df.group_by(keys)
+    .agg(
+        pl.mean("y"),
+        pl.sum("time_run") / 15,
+        pl.mean("time_query"),
+        pl.max("peak_ram"),
+    )
+    .with_columns(total_runtime=pl.col("time_query") + pl.col("time_run"))
+)
 data = res.to_pandas()
-
 y_var = "y"
 
 map_xlabel = {
     "time_run": "Run time (s)",
     "peak_fit": "Peak RAM (GB)",
     "max_ram": "Peak RAM (GB)",
-    "total_time": "Retrieval + Training time (s)",
+    "total_runtime": "Retrieval + Training time (s)",
 }
 
-plot_vars = ["time_run", "max_ram"]
-    
+legend_handles = {}
+legend_labels = {}
+
+# %% TIME PLOTS
+fig = plt.figure(figsize=(10, 1.5))
+
+subfigs = fig.subfigures(1, 3)
+
 # Time plots
-idx_row = 1
-x_var = plot_vars[0]
+x_var = "total_runtime"
 for idx_col in range(3):
-    continue
     grouping_var = grouping_variables[idx_col]
-    ax_left = axs_top[idx_col]
-    ax_left.set_ylim([-0.5, 0.6])
-    ax_left.axhspan(0, -0.5, zorder=0, alpha=0.05, color="red")
-    if idx_col == 1:
-        _xlabel = map_xlabel[x_var]
-    else:
-        _xlabel = ""
+
+    subfig = subfigs[idx_col]
+    ax_ = subfig.subplots(1, 1, squeeze=True)
+    ax_.set_ylim([-0.5, 0.6])
+    ax_.axhspan(0, -0.5, zorder=0, alpha=0.05, color="red")
+
+    if idx_col > 0:
+        ax_.tick_params(labelleft=False)
+
     (h, l), optimal_y = plotting.pareto_frontier_plot(
         data,
         x_var,
@@ -125,139 +126,118 @@ for idx_col in range(3):
         hue_var=grouping_variables[idx_col],
         palette="tab10",
         hue_order=hue_order[grouping_var],
-        ax=ax_left,
+        ax=ax_,
         ax_title="",
-        ax_xlabel=_xlabel,
+        ax_xlabel="",
     )
     l = [constants.LABEL_MAPPING[grouping_variables[idx_col]][_] for _ in l]
 
-    # first row
-    if idx_row == 1:
-        ax_left.set_xscale("log")
+    ax_.set_xscale("log")
 
-        ax_left.legend(
-            h,
-            l,
-            loc="upper center",
-            bbox_to_anchor=(0.5, 1.7),
-            title=constants.LABEL_MAPPING["variables"][grouping_var],
-            ncols=2,
-            mode="expand",
-            edgecolor="white",
-        )
-        ax_left.xaxis.set_major_formatter(major_time_formatter)
-        ax_left.xaxis.set_major_locator(major_time_locator)
-    else:
-        ax_left.get_legend().remove()
-        # ax.legend([], [], edgecolor="white")
-        ax_left.set_xscale("log")
+    ax_.xaxis.set_major_formatter(major_time_formatter)
+    ax_.xaxis.set_major_locator(major_time_locator)
 
-        ax_left.xaxis.set_major_formatter(major_gb_formatter)
-        ax_left.xaxis.set_major_locator(gb_locator)
-        # ax.xaxis.set_minor_formatter(minor_gb_formatter)
+    legend_handles[grouping_var] = h
+    legend_labels[grouping_var] = l
 
-ax_l = axs_top[0]
+    ax_.get_legend().remove()
+
+# Adding annotation to the first plot
+subfig = subfigs[0]
+ax_l = subfig.get_axes()[0]
+
 ax_l.annotate(
     "Pareto\nfrontier",
     (3600, optimal_y),
-    (8000, 0.35),
+    (8000, -0.30),
     fontsize="x-small",
     verticalalignment="center",
     horizontalalignment="center",
-    #   backgroundcolor="white",
-    #   arrowprops=dict(facecolor='black', shrink=0.01, width=1, connectionstyle="arc3,rad=-0.4"),
 )
 
 ax_l.annotate(
     "",
     (3000, optimal_y),
-    (5000, 0.35),
+    (5000, -0.30),
     fontsize="x-small",
     verticalalignment="center",
     horizontalalignment="center",
-    #   backgroundcolor="white",
     arrowprops=dict(
         facecolor="black", shrink=0.01, width=1, connectionstyle="arc3,rad=-0.4"
     ),
 )
+fig.savefig("images/pareto_comparison_with_query_time.png", bbox_inches="tight")
+fig.savefig("images/pareto_comparison_with_query_time.pdf", bbox_inches="tight")
 
 
-#%%
-fig = plt.figure(figsize=(10, 3))
-# fig.set_layout_engine("constrained")
+# %% # RAM plots
+fig = plt.figure(figsize=(10, 1.5))
 
-subfigs = fig.subfigures(1,3, wspace=0.05)
-
-
-# RAM plots
-idx_row = 2
-x_var = plot_vars[idx_row-1]
+# Creating one subfigure for each variable
+subfigs = fig.subfigures(1, 3, wspace=0.05)
+x_var = "peak_ram"
 for idx_col in range(0, 3):
     this_fig = subfigs[idx_col]
-    
-    axs = this_fig.subplots(1,2, sharey=True, width_ratios=[3,1])
+    grouping_var = grouping_variables[idx_col]
+
+    # The subfigure is split in two for the broken axis
+    axs = this_fig.subplots(1, 2, sharey=True, width_ratios=[3, 1])
     this_fig.subplots_adjust(wspace=0.05)
 
-    ax_left=axs[0]
-    ax_right=axs[1]
-    _var = idx_col
-    grouping_var = grouping_variables[_var]
-    ax_left.set_ylim([-0.5, 0.6])
-    
-    ax_left.set_xlim([8000, 21000])
+    # Specifying lims for the axes
+    ax_ = axs[0]
+    ax_right = axs[1]
+    ax_.set_ylim([-0.5, 0.6])
+
+    ax_.set_xlim([8000, 21000])
     ax_right.set_xlim([140000, 150000])
-    
-    ax_left.axhspan(0, -0.5, zorder=0, alpha=0.05, color="red")
+
+    ax_.axhspan(0, -0.5, zorder=0, alpha=0.05, color="red")
     ax_right.axhspan(0, -0.5, zorder=0, alpha=0.05, color="red")
 
-    _xlabel = ""
-    
+    # Removing tick labels from all figures except the leftmost
     if idx_col > 0:
-        ax_left.tick_params(labelleft=False)
-    
-    # if idx_col == 1:
-    #     _xlabel = map_xlabel[x_var]
-    # else:
-    #     _xlabel = ""
-    this_fig.supxlabel(_xlabel)
-    
+        ax_.tick_params(labelleft=False)
+
     (h, l), optimal_y = plotting.pareto_frontier_plot(
         data,
         x_var,
         y_var,
-        hue_var=grouping_variables[_var],
+        hue_var=grouping_variables[idx_col],
         palette="tab10",
         hue_order=hue_order[grouping_var],
-        ax=ax_left,
+        ax=ax_,
         ax_title="",
         ax_xlabel="",
     )
-    
+
     (h, l), optimal_y = plotting.pareto_frontier_plot(
         data,
         x_var,
         y_var,
-        hue_var=grouping_variables[_var],
+        hue_var=grouping_variables[idx_col],
         palette="tab10",
         hue_order=hue_order[grouping_var],
         ax=ax_right,
         ax_title="",
         ax_xlabel="",
     )
-    
-    l = [constants.LABEL_MAPPING[grouping_variables[_var]][_] for _ in l]
 
-    ax_left.get_legend().remove()
+    l = [constants.LABEL_MAPPING[grouping_variables[idx_col]][_] for _ in l]
+
+    # Removing legend
+    ax_.get_legend().remove()
     ax_right.get_legend().remove()
 
-
+    # Prettying up the broken axes 
     # Hiding the axis edges to make it look seamless
-    ax_left.spines.right.set_visible(False)
+    ax_.spines.right.set_visible(False)
     ax_right.spines.left.set_visible(False)
-    # ax_left.xaxis.tick_top()
     ax_right.tick_params(labelleft=False)  # don't put tick labels at the top
     ax_right.tick_params(left=False)  # Hide the ticks on the left side of ax_right
-    ax_right.tick_params(labelleft=False)  # Hide the tick labels on the left side of ax_right
+    ax_right.tick_params(
+        labelleft=False
+    )  # Hide the tick labels on the left side of ax_right
 
     # Prepare the markers
     d = 0.5  # proportion of vertical to horizontal extent of the slanted line
@@ -270,24 +250,62 @@ for idx_col in range(0, 3):
         mew=1,
         clip_on=False,
     )
-    ax_left.plot([1, 1], [1, 0], transform=ax_left.transAxes, **kwargs)
+    ax_.plot([1, 1], [1, 0], transform=ax_.transAxes, **kwargs)
     ax_right.plot([0, 0], [0, 1], transform=ax_right.transAxes, **kwargs)
 
+    # Axis formatting
+    ax_.set_xscale("log")
 
-
-    # ax.legend([], [], edgecolor="white")
-    ax_left.set_xscale("log")
-
-    ax_left.xaxis.set_major_formatter(major_gb_formatter)
-    ax_left.xaxis.set_major_locator(gb_locator)
+    ax_.xaxis.set_major_formatter(major_gb_formatter)
+    ax_.xaxis.set_major_locator(gb_locator)
     ax_right.xaxis.set_major_formatter(major_gb_formatter)
     ax_right.xaxis.set_major_locator(gb_locator)
 
-    ax_left.xaxis.set_minor_locator(NullLocator())
+    ax_.xaxis.set_minor_locator(NullLocator())
 
-fig.supxlabel("Peak RAM (GB)",x=0.5, y=-0.15, fontsize=20, ha='center')
+fig.savefig("images/pareto_comparison_with_query_ram.png", bbox_inches="tight")
+fig.savefig("images/pareto_comparison_with_query_ram.pdf", bbox_inches="tight")
 
-# fig.savefig("images/pareto_comparison_with_query.png",bbox_inches="tight" )
-# fig.savefig("images/pareto_comparison_with_query.pdf", bbox_inches="tight")
+# %% LEGEND
+fig = plt.figure(figsize=(10, 1))
+
+subfigs = fig.subfigures(1, 3)
+
+for idx in range(3):
+    grouping_var = grouping_variables[idx]
+
+    # Extract the colors associated with each hue category
+    colors = sns.color_palette("tab10", len(hue_order[grouping_var]))
+
+    # Create the mapping of hue label to color
+    color_label_mapping = {
+        label: color for label, color in zip(hue_order[grouping_var], colors)
+    }
+
+    # Create a custom legend with Line2D objects
+    _legend_handles = [
+        Line2D([0], [0], color=color, lw=2, marker="o", markersize=8, label=label)
+        for label, color in color_label_mapping.items()
+    ]
+    _legend_labels = legend_labels[grouping_var]
+
+    # Create a new figure for the legend
+    figlegend = subfigs[idx]
+    # Add the legend to the new figure
+    ax_ = figlegend.subplots(1, 1, squeeze=True)
+    ax_.legend(
+        _legend_handles,
+        _legend_labels,
+        loc="center",
+        fontsize=12,
+        ncols=2,
+        title=constants.LABEL_MAPPING["variables"][grouping_var],
+        frameon=False,
+    )
+    ax_.axis("off")
+
+fig.savefig("images/pareto_comparison_with_query_legend.png", bbox_inches="tight")
+fig.savefig("images/pareto_comparison_with_query_legend.pdf", bbox_inches="tight")
+
 
 # %%
