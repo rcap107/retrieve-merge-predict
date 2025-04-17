1cc5b1d592aa25819b98fb057d8af01853021e66
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-04-17 11:21:19 +0200

    cleanup and reformatting

--- scripts/evaluation/summary_results.py
+++ scripts/evaluation/summary_results.py
@@ -1,89 +1,19 @@
-"""This script is used to build the ablation table to compare the performance of each 
-configuration against the reference. 
+"""This script is used to build the ablation table to compare the performance of each
+configuration against the reference.
 
 """
 
 # %%
-# %cd ~/bench
-# %%
+import os
+
+os.chdir("../..")
 import polars as pl
 
 from src.utils.constants import GROUPING_KEYS, REFERENCE_CONFIG
 
-# %%
-df_aggregation = pl.read_parquet("results/results_aggregation.parquet")
-df_general = pl.read_parquet("results/results_general.parquet")
-df_retrieval = pl.read_parquet("results/results_retrieval.parquet")
-df_master = pl.read_parquet("results/master_list.parquet")
-
-# %% TOTAL RUNTIME IN SECONDS
-time_ = (
-    df_master.group_by(GROUPING_KEYS)
-    .agg(pl.col("time_run").mean())
-    .group_by("chosen_model")
-    .agg(pl.col("time_run").sum())
-)
-time_.write_csv("results/total_time_final.csv")
-# %%
-# TRIMMED MEAN
-cutby = 0.2
-
-_d = (
-    df_general.group_by(GROUPING_KEYS)
-    .agg(pl.col("prediction_metric").last())
-    .group_by(["base_table", "target_dl"])
-    .agg(pl.col("prediction_metric").sort())
-    .with_columns(len_l=pl.col("prediction_metric").list.len())
-    .with_columns(
-        start=(pl.col("len_l") * cutby).cast(pl.Int32),
-        end=pl.col("len_l") - (pl.col("len_l") * cutby * 2).cast(pl.Int32),
-    )
-    .with_columns(
-        trimmed=(pl.col("prediction_metric").list.slice(pl.col("start"), pl.col("end")))
-    )
-    .with_columns(
-        trimmed_mean=pl.col("trimmed").list.mean(),
-        trimmed_std=pl.col("trimmed").list.std(),
-    )
-)
-# %%
-_d.select("base_table", "target_dl", "trimmed_mean", "trimmed_std").pivot(
-    values=["trimmed_mean", "trimmed_std"], index="base_table", on="target_dl"
-).sort("base_table").write_csv("trimmed_mean.csv")
-
-# %%
-_d.to_pandas().pivot_table(
-    values=["trimmed_mean", "trimmed_std"],
-    index="base_table",
-    columns="target_dl",
-    aggfunc="mean",
-).to_csv("trimmed_mean.csv")
-# %%
-dedup = (
-    df_general.group_by(GROUPING_KEYS + ["source_table"])
-    .agg(pl.col("prediction_metric").mean(), pl.col("time_run").mean())
-    .with_columns(base_table=pl.col("source_table"))
-)
-
-df_reference = dedup.filter(**REFERENCE_CONFIG)
-df_reference.write_csv("results/results_reference.csv")
-
-# %%
-df_retrieval = df_retrieval.filter(pl.col("estimator") != "nojoin").with_columns(
-    time_run=pl.col("time_run") * 10 + pl.col("time_query")
-)
-df_general = df_general.filter(pl.col("estimator") != "nojoin").with_columns(
-    time_run=pl.col("time_run") * 10 + pl.col("time_query")
-)
-df_aggregation = df_aggregation.filter(pl.col("estimator") != "nojoin").with_columns(
-    time_run=pl.col("time_run") * 10 + pl.col("time_query")
-)
+_gk = [_ for _ in GROUPING_KEYS if _ != "fold_id"]
 
 
-# %%
-
-
-# fold vs fold difference
 def diff_fold_vs_fold(df, key):
     all_variables = [k for k in REFERENCE_CONFIG.keys()]
 
@@ -104,46 +34,101 @@ def diff_fold_vs_fold(df, key):
     return r
 
 
-def diff_mean_vs_mean(df: pl.DataFrame, key):
-    all_variables = [k for k in REFERENCE_CONFIG.keys()]
-    grouping_nofold = [_ for _ in GROUPING_KEYS if _ != "fold_id"]
-
-    _df = df.group_by(grouping_nofold).agg(
-        pl.mean("prediction_metric"), pl.mean("time_run")
+def get_total_runtime(df_master):
+    time_ = (
+        df_master.group_by(GROUPING_KEYS)
+        .agg(pl.col("time_run").mean())
+        .group_by("chosen_model")
+        .agg(pl.col("time_run").sum())
     )
-    r = (
-        _df.join(_df.filter(**REFERENCE_CONFIG), on=key)
+    time_.write_csv("results/total_time_final.csv")
+
+
+def trimmed_mean(df_, cutby=0.2):
+
+    _d = (
+        df_.group_by(GROUPING_KEYS)
+        .agg(pl.col("prediction_metric").last())
+        .group_by(["base_table", "target_dl"])
+        .agg(pl.col("prediction_metric").sort())
+        .with_columns(len_l=pl.col("prediction_metric").list.len())
         .with_columns(
-            diff_metric=pl.col("prediction_metric") - pl.col("prediction_metric_right"),
-            diff_time=pl.col("time_run") / pl.col("time_run_right"),
+            start=(pl.col("len_l") * cutby).cast(pl.Int32),
+            end=pl.col("len_l") - (pl.col("len_l") * cutby * 2).cast(pl.Int32),
         )
-        .group_by(all_variables)
-        .agg(
-            metric_median=pl.median("diff_metric") * 100,
-            time_median=pl.median("diff_time"),
-            metric_mean=pl.mean("diff_metric") * 100,
+        .with_columns(
+            trimmed=(
+                pl.col("prediction_metric").list.slice(pl.col("start"), pl.col("end"))
+            )
         )
-        .sort("metric_median", descending=True)
+        .with_columns(
+            trimmed_mean=pl.col("trimmed").list.mean(),
+            trimmed_std=pl.col("trimmed").list.std(),
+        )
     )
-    return r
+
+    return _d
 
 
+def prepare_reference(df_general):
+
+    dedup = (
+        df_general.group_by(GROUPING_KEYS + ["source_table"])
+        .agg(pl.col("prediction_metric").mean(), pl.col("time_run").mean())
+        .with_columns(base_table=pl.col("source_table"))
+    )
+
+    df_reference = dedup.filter(**REFERENCE_CONFIG)
+    df_reference.write_csv("results/results_reference.csv")
+
+
+def prepare_full_time_run(df):
+    _d = df.group_by(_gk).agg(pl.sum("time_run")).select(_gk + ["time_run"])
+
+    df = (
+        df.drop("time_run")
+        .join(_d, on=_gk)
+        .with_columns(time_run=pl.col("time_run") + pl.col("time_query"))
+    )
+
+    return df
+
+
+# %%
+df_aggregation = pl.read_parquet("results/results_aggregation.parquet")
+df_general = pl.read_parquet("results/results_general.parquet")
+df_retrieval = pl.read_parquet("results/results_retrieval.parquet")
+df_master = pl.read_parquet("results/master_list.parquet")
+
+# Reformatting time run so that it includes all folds + query time
+df_retrieval = prepare_full_time_run(df_retrieval)
+df_general = prepare_full_time_run(df_general)
+df_aggregation = prepare_full_time_run(df_aggregation)
+
+# %%
+# TRIMMED MEAN
+df_trimmed = trimmed_mean(df_general, cutby=0.2)
+# The table is exported and edited in google sheets for easier formatting
+df_trimmed.to_pandas().pivot_table(
+    values=["trimmed_mean", "trimmed_std"],
+    index="base_table",
+    columns="target_dl",
+    aggfunc="mean",
+).to_csv("trimmed_mean.csv")
+# fold vs fold difference
 # %%
 # retrieval method
 target = "jd_method"
 this_key = [_ for _ in GROUPING_KEYS if _ != target]
 this_groupby = [_ for _ in GROUPING_KEYS if not _ in [target, "fold_id"]]
 _1 = diff_fold_vs_fold(df_retrieval, this_key)
-_1_mean = diff_mean_vs_mean(df_retrieval, this_groupby)
 _1
-
 # %%
 # selector
 target = "estimator"
 this_key = [_ for _ in GROUPING_KEYS if _ != target]
 this_groupby = [_ for _ in GROUPING_KEYS if not _ in [target, "fold_id"]]
 _2 = diff_fold_vs_fold(df_general, this_key)
-_2_mean = diff_mean_vs_mean(df_general, this_groupby)
 _2
 # %%
 # ml model
@@ -151,7 +136,6 @@ target = "chosen_model"
 this_key = [_ for _ in GROUPING_KEYS if _ != target]
 this_groupby = [_ for _ in GROUPING_KEYS if not _ in [target, "fold_id"]]
 _3 = diff_fold_vs_fold(df_general, this_key)
-_3_mean = diff_mean_vs_mean(df_general, this_groupby)
 _3
 
 
@@ -161,10 +145,10 @@ target = "aggregation"
 this_key = [_ for _ in GROUPING_KEYS if _ != target]
 this_groupby = [_ for _ in GROUPING_KEYS if not _ in [target, "fold_id"]]
 _4 = diff_fold_vs_fold(df_aggregation, this_key)
-_4_mean = diff_mean_vs_mean(df_aggregation, this_groupby)
 _4
 
 # %%
+# These tables are formatted in google sheets
 _1.write_csv("results/diff_from_ref/retrieval_method.csv")
 _2.write_csv("results/diff_from_ref/selector.csv")
 _3.write_csv("results/diff_from_ref/ml_model.csv")
